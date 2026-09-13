c6fb911ed81b6fc95040c6e92bd064acf6a3fd23
namacman <soft.acman@gmail.com>
2026-09-13 10:14:29 +0700

    init project

---
+++ Terraform/backend.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "~> 1.13.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.14.1"
+    }
+  }
+
+  cloud {}
+}
+provider "aws" {
+  region = var.aws-region
+}
